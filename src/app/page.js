f0b92d9783b68cee8e621ec37ddc4524bb7fcb0c
Rajan-007 <aravinthrajan034@gmail.com>
2023-12-20 21:39:32 +0530

    local storage

--- src/app/page.js
+++ src/app/page.js
@@ -1,46 +1,64 @@
-"use client"
-import React,{ useState } from "react"
+"use client";
+import React, { useState, useEffect } from "react";
 
+const Home = () => {
+  const [todos, setTodos] = useState(() => localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : []);
+  const [task, setTask] = useState('');
 
-export default function Home() {
-
-  const[todos,setTodos]=useState([]);
-  const[task,setTask]=useState(' ');
-
-  const addTodo = () => {
-    if (task.trim()){
-      setTodos([...todos , task])
-     
+  const handleAddTodo = async () => {
+    if (task.trim()) {
+      setTodos([...todos, task]);
+      await localStorage.setItem('todos', JSON.stringify(todos));
+      setTask(''); // Clear input
+  
+      // Force a re-render to ensure the UI reflects the latest state
+      setTodos((prevTodos) => prevTodos);
     }
-  }
-
-  const Delete = (itemindex) => {
-    setTodos(todos.filter((todo,index ) => index !== itemindex));
   };
   
 
+  const handleDeleteTodo = async (index) => {
+    setTodos(todos.filter((todo, todoIndex) => todoIndex !== index));
+    await localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
   return (
-    <div className='bg-gray-900 min-h-screen '>
-      <h1 className='bg-black text-center text-3xl  font-bold py-5 '>Todo list</h1>
-      <div className='flex justify-center my-10'>
-        <input className='text-blue-500 p-2 text-lg' type="text" onChange={(e) => setTask(e.target.value)} ></input>
-        <button className=' bg-emerald-800 p-4 mx-4 rounded text-xl font-bold' onClick={addTodo}> Add </button>
+    <div className="bg-gray-900 min-h-screen">
+      <h1 className="bg-black text-center text-3xl font-bold py-5">Todo List</h1>
+      <div className="flex justify-center my-10">
+        <input
+          className="text-blue-500 p-2 text-lg"
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button
+          className="bg-emerald-800 p-4 mx-4 rounded text-xl font-bold"
+          onClick={handleAddTodo}
+        >
+          Add
+        </button>
       </div>
       <div className="flex justify-center items-center">
         <ul>
-          {todos.map((todo,index) => (
+          {todos.map((todo, index) => (
             <li key={todo} className="font-semibold text-2xl bg-black p-4 px-6 m-5 rounded-sm">
               <label>
-                <input className="mr-8 h-8 w-8   "
-                  type="checkbox"
-                />
+                <input className="mr-8 h-8 w-8" type="checkbox" />
                 {todo}
-                <button className="bg-white text-black text-lg rounded-full ml-10 p-2" onClick={ () => Delete(index)} >Remove</button>
+                <button
+                  className="bg-white text-black text-lg rounded-full ml-10 p-2"
+                  onClick={() => handleDeleteTodo(index)}
+                >
+                  Remove
+                </button>
               </label>
             </li>
           ))}
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
